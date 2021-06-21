4a41331d87e960099816a874e80131c9865a36fe
Umit Turk <uyt.turk@gmail.com>
2021-06-22 02:02:26 +0300

    productfeed, product component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -14,8 +15,21 @@ export default function Home() {
      <main className="max-w-screen-2xl mx-auto">
        <Banner />
 
-       {/* ProductFeed */}
+       <ProductFeed products={products}/>
      </main>
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props : {
+      products,
+  }}
+}
+
+// get >>>  https://fakestoreapi.com/products
