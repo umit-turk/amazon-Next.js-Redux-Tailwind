6123ef8aa660e2e34ce12da9313f976bb4dca3d1
Umit Turk <uyt.turk@gmail.com>
2021-06-22 00:36:51 +0300

    right of the section header, bottom navbar, banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,11 +24,45 @@ function Header() {
                     <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
                     <SearchIcon className="h-12 p-4 " />
                 </div>
+                {/* Right */}
+                <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                    <div className=" link">
+                        <p>Hello Umit</p>
+                        <p className="font-extrabold md:text-sm">Account & Lists</p>
+                    </div>
+
+                    <div className=" link">
+                        <p>Returns</p>
+                        <p className="font-extrabold md:text-sm">& Orders</p>
+                    </div>
+
+                    <div className="relative link flex items-center ">
+
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+
+                        <ShoppingCartIcon className="h-10"/>
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+                    </div>
+                </div>
 
             </div>
 
             {/* Bottom Nav */}
-            <div></div>
+            <div className="flex items-center space-x-3 p-2 pl-6 text-white bg-amazon_blue-light text-wite text-sm">
+                <p className="link flex items-center">
+                <MenuIcon className="h-6 mr-1"/>    
+                All
+                </p>
+                <p className="link">Prime Video</p>
+                <p className="link">Amazon Business</p>
+                <p className="link">Today's Deals</p>
+                <p className="link hidden lg:inline-flex">Electronics</p>
+                <p className="link hidden lg:inline-flex">Food & Grocery</p>
+                <p className="link hidden lg:inline-flex">Prime</p>
+                <p className="link hidden lg:inline-flex">Buy Again</p>
+                <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+                <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+            </div>
         </header>
     )
 }
