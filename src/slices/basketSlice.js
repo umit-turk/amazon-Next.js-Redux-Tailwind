36c832694691d59e91928a12b8fe7437c06308c8
Umit Turk <uyt.turk@gmail.com>
2021-06-23 01:00:59 +0300

    checkout, checkoutProduct, add/remove btns

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -10,9 +10,24 @@ export const basketSlice = createSlice({
   reducers: {
     //Actions
     addToBasket: (state, action) => {
-      state.items = [...state.items, action.payload]
+      state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (basketItem) => basketItem.id === action.payload.id
+      );
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        //The item exists in the basket... remove it
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.payload.id}) as it is not in the basket`
+        );
+      }
+      state.items = newBasket;
+    },
   },
 });
 
@@ -20,5 +35,6 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
-
+export const selectTotal = (state) =>
+  state.basket.items.reduce((total, item) => total + item.price, 0);
 export default basketSlice.reducer;
