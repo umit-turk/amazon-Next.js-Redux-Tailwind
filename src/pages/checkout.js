2cb33ab471ff5d407e7c48135ad25a454d7f03ec
Umit Turk <uyt.turk@gmail.com>
2021-06-23 19:42:19 +0300

    stripe checkout ,webhook

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,11 +5,32 @@ import { useSelector } from "react-redux";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    //Call the backend to create a checkout session...
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    });
+
+    // Redirect user/customer to Stripe Checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
 
   return (
     <div className="bg-gray-100">
@@ -55,12 +76,14 @@ function Checkout() {
               <h2 className="whitespace-nowrap">
                 Subtotal ({items.length} items): {""}
                 <span className="font-bold">
-                   <Currency quantity={total} currency="GBP"/>
+                  <Currency quantity={total} currency="GBP" />
                 </span>
               </h2>
 
               <button
-              disabled={!session}
+                role="link"
+                onClick={createCheckoutSession}
+                disabled={!session}
                 className={`button mt-2 ${
                   !session &&
                   "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
